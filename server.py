4aeb5aecd560ea7aa74160117e50aa35ad9a2409
David3186 <72481383+David3186@users.noreply.github.com>
2023-09-10 20:31:08 -0400

    Use free GPT 4

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from single_qa_agent import SingleQaAgent
 from langchain.chat_models import ChatOpenAI
 import json
 import prompts
+import requests
 
 app = Flask(__name__)
 
@@ -17,13 +18,68 @@ class InteractiveAgentSession():
                           vectorstore_sim_score_threshold=0.7,
                           passages_path="./data/dev_david/cs_data.pkl",
                           tfidf_k=10)
+        
+    def make_request(self, user_input):
+        url = 'https://chatgptspanish.org/wp-admin/admin-ajax.php'
+        headers = {
+            'Accept': '*/*',
+            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryVTFT3tmNveC68dAT',
+            'Origin': 'https://chatgptspanish.org',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Host': 'chatgptspanish.org',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15',
+            'Referer': 'https://chatgptspanish.org/',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+        }
+
+        post_request_data = f'''------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="_wpnonce"
+
+bd435bd251
+------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="post_id"
+
+44
+------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="url"
+
+https://chatgptspanish.org
+------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="action"
+
+wpaicg_chat_shortcode_message
+------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="message"
+
+Speak english. {user_input}
+
+------WebKitFormBoundaryVTFT3tmNveC68dAT
+Content-Disposition: form-data; name="bot_id"
+
+0
+------WebKitFormBoundaryVTFT3tmNveC68dAT--
+'''
+
+        response = requests.post(url, headers=headers, data=post_request_data)
+        
+        return response.json()["data"]
+
 
     def ask_agent(self, req):
         # Adds data from python client request
+        # try:
+        #     question = req.get_json().get('question')
+        #     response = self.agent.ask(question)
+        #     return json.dumps({'status': 200, 'success': response.get('output_text')})
+        # except:
+        #     print("Unable to get client data.")
+        #     return json.dumps({'status': 500, 'success': "Internal Server Error"})
+
         try:
             question = req.get_json().get('question')
-            response = self.agent.ask(question)
-            return json.dumps({'status': 200, 'success': response.get('output_text')})
+            response = self.make_request(question)
+            return json.dumps({'status' : 200, 'success': response})
         except:
             print("Unable to get client data.")
             return json.dumps({'status': 500, 'success': "Internal Server Error"})
